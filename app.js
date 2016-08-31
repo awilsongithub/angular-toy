2665b6d4e57408706a3a8c957b949c04bbc10f87
Adam Wilson <adamwilson1234@gmail.com>
2016-08-31 17:30:01 -0500

    slowly rebuilding flatlander as angular-toy

--- app.js
+++ app.js
@@ -4,9 +4,19 @@
 (function(){
 
 //module. store-products dependency is another module
-var app = angular.module('gemStore', ['store-products']);
+var app = angular.module('gemStore');
 
+// store is the controller and we will add properties to it
+// and we will put our data in the products property
+app.controller('StoreController', ['$http', function($http){
+    var store = this;
+    store.products = [ ];
+    $http.get('/products.json').success(function(data){
+        console.log(data);
+        store.products = data;
+    });
 
+}]);
 
 
 
