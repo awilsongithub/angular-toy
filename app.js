ab6739b5ef1c4be601a3278468b6584839535369
Adam Wilson <adamwilson1234@gmail.com>
2016-08-31 16:36:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+// put all in closure
+(function(){
+
+//module. store-products dependency is another module
+var app = angular.module('gemStore', ['store-products']);
+
+
+
+
+
+
+
+
+
+})(); // end closure
